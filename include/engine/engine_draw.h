28794c8137c7ca333955f29f2b545e0c291ef43a
Jacob Brown <Jake@insphereltd.com>
2022-12-02 16:26:10 +0000

    Added rectangle drawing code and profile test.

--- include/engine/engine_draw.h
+++ include/engine/engine_draw.h
@@ -3,7 +3,7 @@
 #include "engine_screen.h"
 #include "engine_color.h"
 
-inline void draw_clear_screen16_inline(
+inline void draw_clear_screen16(
 	const screen_buffer* const screen,
 	const uint16_t color)
 {
@@ -16,7 +16,7 @@ inline void draw_clear_screen16_inline(
 	}
 }
 
-inline void draw_clear_screen32_inline(
+inline void draw_clear_screen32(
 	const screen_buffer* const screen,
 	const uint32_t color)
 {
@@ -29,7 +29,7 @@ inline void draw_clear_screen32_inline(
 	}
 }
 
-inline void draw_clear_screen64_inline(
+inline void draw_clear_screen64(
 	const screen_buffer* const screen,
 	const uint64_t color)
 {
@@ -39,5 +39,95 @@ inline void draw_clear_screen64_inline(
 	for (int i = 0; i < pixCount; i++)
 	{
 		pix[i] = color;
+	}
+}
+
+inline void draw_filled_rect16(
+	const screen_buffer* const screen,
+	const uint16_t color,
+	int x, int y, 
+	int w, int h)
+{
+	uint16_t* pix = (uint16_t*)screen->pixels;
+
+	for (int j = y; j < y + h; j++)
+	{
+		for (int i = x; i < x + w; i++)
+		{
+			int pixelIndex = (screen->width * j) + i;
+			pix[pixelIndex] = color;
+		}
+	}
+}
+
+inline void draw_filled_rect32(
+	const screen_buffer* const screen,
+	const uint32_t color,
+	int x, int y,
+	int w, int h)
+{
+	uint32_t* pix = (uint32_t*)screen->pixels;
+
+	for (int j = y; j < y + h; j++)
+	{
+		for (int i = x; i < x + w; i++)
+		{
+			int pixelIndex = (screen->width * j) + i;
+			pix[pixelIndex] = color;
+		}
+	}
+}
+
+inline void draw_unfilled_rect16(
+	const screen_buffer* const screen,
+	const uint16_t color,
+	int x, int y,
+	int w, int h)
+{
+	uint16_t* pix = (uint16_t*)screen->pixels;
+	int pixelIndex = (screen->width * x) + y;
+	int offset = h * screen->width;
+
+	for (int i = x; i < x + w; i++)
+	{
+		pix[pixelIndex] = color;
+		pix[pixelIndex + offset] = color;
+		pixelIndex++;
+	}
+
+	pixelIndex = (screen->width * x) + y;
+
+	for (int i = y; i < y + h; i++)
+	{
+		pix[pixelIndex] = color;
+		pix[pixelIndex + w] = color;
+		pixelIndex += screen->width;
+	}
+}
+
+inline void draw_unfilled_rect32(
+	const screen_buffer* const screen,
+	const uint32_t color,
+	int x, int y,
+	int w, int h)
+{
+	uint32_t* pix = (uint32_t*)screen->pixels;
+	int pixelIndex = (screen->width * x) + y;
+	int offset = h * screen->width;
+
+	for (int i = x; i < x + w; i++)
+	{
+		pix[pixelIndex] = color;
+		pix[pixelIndex + offset] = color;
+		pixelIndex++;
+	}
+
+	pixelIndex = (screen->width * x) + y;
+
+	for (int i = y; i < y + h; i++)
+	{
+		pix[pixelIndex] = color;
+		pix[pixelIndex + w] = color;
+		pixelIndex += screen->width;
 	}
 }
